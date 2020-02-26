a39b196ba65cac2f3f16666830fa77de42eb87e2
cleucht <c.leucht@inpsyde.com>
2020-02-26 11:16:55 +0100

    add missing PluginConfigurationBuilder.

---
+++ src/Config/PluginConfigurationBuilder.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1); # -*- coding: utf-8 -*-
+
+namespace Inpsyde\WpTranslationDownloader\Config;
+
+use Composer\Json\JsonFile;
+use Composer\IO\IOInterface;
+
+final class PluginConfigurationBuilder
+{
+
+    const KEY = 'wp-translation-downloader';
+
+    /**
+     * @var IOInterface
+     */
+    private $io;
+
+    /**
+     * PluginConfigurationBuilder constructor.
+     *
+     * @param IOInterface $io
+     */
+    public function __construct(IOInterface $io)
+    {
+        $this->io = $io;
+    }
+
+    public function build(array $extra = []): PluginConfiguration
+    {
+        if (! isset($extra[self::KEY])) {
+
+
+
+            return new PluginConfiguration([]);
+        }
+
+        $config = $extra[self::KEY];
+
+        if(is_array($config)){
+            return new PluginConfiguration($config);
+        }
+
+        $file = new JsonFile($config);
+
+        if ($file->exists()) {
+            return new PluginConfiguration($file->read());
+        }
+
+        return new PluginConfiguration([]);
+    }
+}
