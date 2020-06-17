9ff0b86fad399eec81f3612ac8b5ef66f0ea432c
chrico <c.leucht@inpsyde.com>
2020-06-17 08:53:02 +0200

    init commit to support composer 2.

--- src/Plugin.php
+++ src/Plugin.php
@@ -220,6 +220,22 @@ final class Plugin implements
     }
 
     /**
+     * @param Composer $composer
+     * @param IOInterface $io
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
+     * @param Composer $composer
+     * @param IOInterface $io
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    /**
      * @throws \RuntimeException
      */
     private function ensureDirectories()
